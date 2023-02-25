b05ae100faeac0e4e3489decd9eba75a3e32ff6d
Samuel Vincze <samuel125vincze@gmail.com>
2023-02-25 22:01:05 +0100

    Import kniznice  Apache Commons Math. Vytvorenie triedy ValidacneVypocty Naprogramovanie metod pre LOD a LOQ

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,24 @@
-package org.example;public class Main {
+package org.example;
+
+import sk.shmu.oslvalid.ValidacneVypocty;
+import java.util.ArrayList;
+
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Double>nameraneData=new ArrayList<>();
+        nameraneData.add(5.0);
+        nameraneData.add(4.0);
+        nameraneData.add(2.0);
+        nameraneData.add(1.0);
+        nameraneData.add(8.0);
+
+        ValidacneVypocty validacia = new ValidacneVypocty(nameraneData);
+        System.out.println(validacia.vypocetLOD());
+        System.out.println(validacia.vypocetLOQ());
+
+
     }
 }
